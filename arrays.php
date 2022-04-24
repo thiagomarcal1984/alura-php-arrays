14b4a059bd24845a66d1e8855b367c504915457c
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-04-24 18:22:51 -0300

    Funções is_array e array_is_list (testa se o array é usável em um for).

--- arrays.php
+++ arrays.php
@@ -30,3 +30,36 @@ echo "Função count: " . count($array) . PHP_EOL;
 echo "Função sizeof: " . sizeof($array) . PHP_EOL; // Count é um alias para sizeof (ou o contrário).
 
 // var_dump($array);
+
+$array = [
+    1 => 'um',
+    2 => 'dois',
+    3 => 'tres',
+];
+
+echo "Índices [1, 2, 3]: " . var_dump(array_is_list($array)); // É uma lista é sequencial, mesmo começando por 1.
+
+$array = [
+    0 => 'um',
+    1 => 'dois',
+    2 => 'tres',
+];
+
+echo "Índices [0, 1, 2]: " . var_dump(array_is_list($array)); // É uma lista porque começa de 0 e é sequencial.
+
+$array = [
+    0 => 'um',
+    1 => 'dois',
+    3 => 'tres',
+];
+
+echo "Índices [0, 1, 3]: " . var_dump(array_is_list([$array])); // Não é uma lista porque a sequência não foi mantida.
+
+$array = [
+    '0' => 'um',
+    '1' => 'dois',
+    '3' => 'tres',
+];
+
+echo "Índices string [0, 1, 3]: " . var_dump(array_is_list([$array])); // Não é uma lista porque a sequência não foi mantida.
+echo PHP_EOL . "Fim. " . PHP_EOL;
--- notas.php
+++ notas.php
@@ -1,19 +1,6 @@
 <?php
 
 $notas = [
-    10,
-    8,
-    9,
-    7,
-    6,
-];
-
-rsort($notas); // Reverse sorting, inverte a ordeanação.
-var_dump($notas);
-
-
-// Exemplo com array associativo.
-$notas = [
     'Ana' => 10,
     'João' => 8,
     'Maria' => 9,
@@ -21,6 +8,7 @@ $notas = [
     'Vinicius' => 6,
 ];
 
+// rsort = Reverse sorting, inverte a ordenação por valor.
 // asort = Associative Sort, ordena pelo valor do Array associativo, em ordem crescente. Mantém o índice de string.
 // arsort = Associative Reverse Sort, ordena pelo valor do Array associativo, em ordem decrescente. Mantém o índice de string.
 // ksort = Key Sort, ordena pelo índice do Array associativo, em ordem crescente. Mantém o índice de string.
@@ -28,14 +16,14 @@ $notas = [
 krsort($notas); // Reverse sorting, inverte a ordeanação.
 var_dump($notas);
 
-// usort = User sort, ordena pelo valor o Array associativo, em ordem crescente. É necessário fornecer uma função de comparação definida pelo usuário.
-// uksort = User key sort, ordena pela chave o Array associativo, em ordem crescente. É necessário fornecer uma função de comparação definida pelo usuário.
-
-function userSort($nota1, $nota2) {
-    return $nota1 <=> $nota2; // Ordem crescente de índice.
-    // return $nota2 <=> $nota1; // Ordem decrescente de índice.
+// if (gettype($notas) === 'array') // Um meio de comparar se é array.
+if (is_array($notas)) // Forma mais legível de comparar se é um array.
+{
+    echo "Sim, é um array.";
 }
 
-uksort($notas, 'userSort'); // O ksort já é suficiente pra isso.
+echo PHP_EOL;
+echo "Testa se é uma lista indexada com inteiros: " . array_is_list($notas);
 
-var_dump($notas);
+var_dump (array_is_list([])); // Um array vazio é uma lista.
+var_dump (array_is_list($notas)); // Um array associativo não é uma lista.
