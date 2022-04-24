0e9b8ec1bc6b6286112f5ffdbab3b272e40aa01a
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-04-24 15:53:30 -0300

    Acréscimo dos links complementares.

--- arrays.php
+++ arrays.php
@@ -1,15 +1,22 @@
 <?php
 
+/**
+ * Os arrays em PHP na verdade são HashMaps cujas chaves 
+ * são os índices (sejam eles inteiros ou strings).
+ * Ou seja, todos os arrays são associativos em PHP.
+ * 
+ * Link sobre conversão de tipos em PHP: 
+ * https://www.php.net/manual/en/language.types.integer.php#language.types.integer.casting
+ * 
+ * PodCast sobre Tabela Hash.
+ * https://www.hipsters.tech/tabela-de-hash-hipsters-ponto-tech-222/
+ * 
+ *  */ 
+
 $array = [
     1 => 'um',
     true => 'dois',
     1.3 => 'tres',
 ];
-
-/**
- * Os arrays em PHP na verdade são HashMaps cujas chaves 
- * são os índices (sejam eles inteiros ou strings).
- * Ou seja, todos os arrays são associativos em PHP.
- *  */ 
 
 var_dump($array);
