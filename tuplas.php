2d10d23ba09714458598de0fbe83abdf95e30c02
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-04-25 16:15:10 -0300

    Funções extract e compact para declarar variáveis e criar arrays a partir de variáveis.

--- tuplas.php
+++ tuplas.php
@@ -17,11 +17,23 @@ $dados = [
     'idade' => 24
 ];
 
-[$nome, $nota, $idade] = $dados; // Esta atribuição múltipla vai falhar: as variáveis receberão NULL.
-var_dump($nome, $nota, $idade); // Os lists são indexados por inteiros, não por string.
+// [$nome, $nota, $idade] = $dados; // Esta atribuição múltipla vai falhar: as variáveis receberão NULL.
+// var_dump($nome, $nota, $idade); // Os lists são indexados por inteiros, não por string.
 
-['nome' => $nome, 'nota' => $nota, 'idade' => $idade] = $dados; // Esta atribuição múltipla vai funcionar: o índice é mapeado pra variável corretamente.
+// ['nome' => $nome, 'nota' => $nota, 'idade' => $idade] = $dados; // Esta atribuição múltipla vai funcionar: o índice é mapeado pra variável corretamente.
+
+unset($nome, $nota, $idade); // Remove as variáveis.
+extract($dados); // A função extract pega os índices de um array com chaves de string e extrai variáveis.
+// NUNCA USE O COMANDO EXTRACT COM ARRAYS QUE VOCÊ NÃO CONFIA!!!
+
 echo "Nome: $nome   Idade: $idade   Nota: $nota" . PHP_EOL;
+
+// O comando compact faz o inverso: pega os nomes das variáveis 
+// e cria um array com índice de string.
+$ex1 = true;
+$ex2 = 'Dois';
+$ex3 = 3;
+var_dump(compact('ex1', 'ex2', 'ex3'));
 
 // Cópia do exercício:
 
