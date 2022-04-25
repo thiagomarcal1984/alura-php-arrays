f8fdcdc49551daf27eb1f42d836dab361b6ff71a
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-04-25 16:28:48 -0300

    Texto sobre as funções map, filter e reduce.

--- ordem.php
+++ ordem.php
@@ -15,13 +15,26 @@ $notas = [
     ],
 ];
 
-function ordenaNotas(array $nota1, array $nota2) : int 
-{
-    // return $nota2['nota'] - $nota1['nota']; // Função resumida sem o operador espaçonave.
-    return $nota2['nota'] <=> $nota1['nota']; // Operador "espaçonave". 
-    // Quem vem antes do operador aparece primeiro; quem vem depois do operador aparede depois.
-};
+// Escondendo a ordena notas para que a linha do usort use uma função de ordenação anônima.
+// function ordenaNotas(array $nota1, array $nota2) : int 
+// {
+//     // return $nota2['nota'] - $nota1['nota']; // Função resumida sem o operador espaçonave.
+//     return $nota2['nota'] <=> $nota1['nota']; // Operador "espaçonave". 
+//     // Quem vem antes do operador aparece primeiro; quem vem depois do operador aparede depois.
+// };
 
-usort($notas, 'ordenaNotas'); // O usort pega o array e o nome da função de ordenação (formato de string).
+// usort($notas, 'ordenaNotas'); // O usort pega o array e o nome da função de ordenação (formato de string).
+
+usort($notas, function(array $nota1, array $nota2){ // Inserção da função de ordenação (anônima).
+    return $nota2['nota'] <=> $nota1['nota'];
+});
+
+// Link sobre as funções filter, map e reduce:
+// https://www.alura.com.br/artigos/trabalhando-com-arrays-em-php
+// array_filter => retorna um array com os dados que batem com os critérios de filtro.
+// array_map => retorna um novo array cujos elementos foram alterados a partir dos dados do array original.
+// array_reduce => retorna um valor que resume o conteúdo do array (por exemplo, somatório, multiplicatório etc.).
+// Em comum, todas essas funções recebem como parâmetro uma referência ao array original e
+// uma função anônima.
 
 var_dump($notas);
